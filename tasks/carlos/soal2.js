520d98090b0120914f0bbf17b5ac02971794e1fd
carlos <carlosnainggolan@gmail.com>
2026-07-25 17:44:41 +0700

    refactor: add throw error to validate input(data type: number)

--- tasks/carlos/soal2.js
+++ tasks/carlos/soal2.js
@@ -1,6 +1,7 @@
 export const cetakTabel = (n) => {
+
   if (typeof n !== "number" || n < 1) {
-    return "Parameter harus bertipe data nomor dan harus nomor positif";
+    throw new Error ("Parameter harus bertipe data nomor dan harus nomor positif");
   } else {
     let hasil ="";
     for (let i = 1; i <= 10; i++) {

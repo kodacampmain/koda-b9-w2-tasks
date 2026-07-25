2bfcf96f20b0bdf8d8d9043cea261e292d6cd8d9
MGT06 <grdgiven4@gmail.com>
2026-07-25 23:05:00 +0700

    feat: add handled all function in directory given

---
+++ tasks/given/index.js
@@ -0,0 +1,14 @@
+import { manualReduce, callbackReduce } from "./reduce.js";
+import { filter } from "./filter.js";
+import { cetakTabel } from "./cetakTabel.js";
+import { countArray } from "./countArray.js";
+
+const func = {
+  manualReduce,
+  callbackReduce,
+  filter,
+  cetakTabel,
+  countArray,
+};
+
+export default func;

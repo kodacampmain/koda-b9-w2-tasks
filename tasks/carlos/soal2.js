c8cc95a42cfb2cb96627edc394dba9567494c9c7
carlos <carlosnainggolan@gmail.com>
2026-07-25 17:07:28 +0700

    refactor: changing console.log to using return

--- tasks/carlos/soal2.js
+++ tasks/carlos/soal2.js
@@ -1,9 +1,11 @@
 export const cetakTabel = (n) => {
   if (typeof n !== "number" || n < 1) {
-    console.log("Parameter harus bertipe data nomor dan harus nomor positif");
+    return "Parameter harus bertipe data nomor dan harus nomor positif";
   } else {
+    let hasil ="";
     for (let i = 1; i <= 10; i++) {
-      console.log(`${n} x ${i} = ${n*i}`);
+      hasil += `${n} x ${i} = ${n*i}\n`;
     }
+    return hasil;
   }
 };

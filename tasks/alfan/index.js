02ec9fccab1f4e03095276d43f03fe534f37e967
alfan <alfannurfaizwork@gmail.com>
2026-07-25 05:12:06 -0500

    feat: add function to export all function in directory

---
+++ tasks/alfan/index.js
@@ -0,0 +1,13 @@
+import { cetakTabel } from "./cetakTabel.js";
+import { countArray } from "./countArray.js";
+import { customFilter } from "./customFilter.js";
+import { customReduce } from "./customReduce.js";
+
+const func = {
+  cetakTabel,
+  countArray,
+  customFilter,
+  customReduce
+};
+
+export default func;

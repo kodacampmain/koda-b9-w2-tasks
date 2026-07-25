1ace35d895a06dbc3fedf3e0c1140253011cc1ce
carlos <carlosnainggolan@gmail.com>
2026-07-25 17:45:43 +0700

    refactor: add throw error to validate input(data type: array)

--- tasks/carlos/soal3.js
+++ tasks/carlos/soal3.js
@@ -1,4 +1,7 @@
 export const countArray = (arr) => {
+  if (!Array.isArray(arr)) {
+    throw new Error("Parameter harus berupa array");
+  }
   let total = 1;
   for (let i = 0; i < arr.length; i++) {
     total *= arr[i];

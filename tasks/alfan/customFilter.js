cc08657cf86d9d2ad9efad064f0fe5d371391af9
alfan <alfannurfaizwork@gmail.com>
2026-07-26 02:47:32 -0500

    docs: add comment to explain step by step

--- tasks/alfan/customFilter.js
+++ tasks/alfan/customFilter.js
@@ -1,17 +1,25 @@
+// membuat dan mengeksport function
 export const customFilter = (arrs, callback) => {
+  // membuat array kosong untuk menampung nilai dari callback
   const result = [];
 
+  // jika input bukan array maka akan error dan muncul peringatan
   if (!Array.isArray(arrs)) {
     throw new Error("Parameter harus berupa array");
   }
 
+  // membuat loop untuk mengambil masing masing nilai yang ada di dalam array
   for (let i = 0; i < arrs.length; i++) {
+    // membuat variable n untuk mengambil nilai dari masing masing array
     const n = arrs[i];
 
+    // membuat pengkondisian untuk mengambil nilai truthy dari callback
     if (callback(n, i)) {
+      // jika calback dengan argument n,i bernilai true maka result di tambah (push) dengan nilai n
       result.push(n);
     }
   }
 
+  // return result untuk menghasilkan array baru
   return result;
 };

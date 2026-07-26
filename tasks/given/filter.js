83349c98df18b79e4d827777360ec012d32b25fa
MGT06 <grdgiven4@gmail.com>
2026-07-26 15:05:21 +0700

    feat: add callback function for function filter

--- tasks/given/filter.js
+++ tasks/given/filter.js
@@ -1,18 +1,27 @@
-export function filter(arr) { // menerima parameter array
+function filter(arr, callback) { // menerima parameter array dan callback
   if (!Array.isArray(arr)) { // mengecek apakah input benar array
     throw new Error("Input harus berupa array");
   }
   const result = []; // menginisialisasi result sebagai array
   for (let i = 0; i < arr.length; i++) { //melakukan proses filter method
-    const check = arr[i].length;
-    if (check > 6) {
-      result[result.length] = ` ${arr[i]}`;
+    if(callback(arr[i])){ // menegecek apakah callback mengembalikan nilai yang benar
+      result[result.length] = callback(arr[i]); // memasukan nilai yang di kembalikan callback kedalam array resultl
     }
   }
 
-  if(result.length === 0){ // mengembalikan pesan error jika tidak ada hasil yang sesuai
-    throw new Error ("Kata dengan huruf lebih dari 6 tidak ditemukan");
+  if (result.length === 0) { // mengembalikan pesan error jika tidak ada hasil yang sesuai
+    throw new Error("Kata dengan huruf lebih dari 6 tidak ditemukan");
   }
   console.log("\n-----Manual Filter-----\n");
   return result;
 }
+
+function callbackFilter(ele) { // memmbuat callbackFilter yang menerima parameter elemen
+  const check = ele.length; // membuat variabel check yang mengambil panjang dari elemen
+  if (check > 6) { // mengecek panjang dari elememen
+    return ele; // mengembalikan elemen yang sudah sesaui
+  }
+  return ;
+}
+
+console.log(filter(words, callbackFilter))

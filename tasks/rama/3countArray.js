a4ff69091c80cc9638c8d76db5c08c44ee27721a
Rama Lana Komara <rlanakomara7@gmail.com>
2026-07-27 09:44:42 +0700

    feat: add error handling

--- tasks/rama/3countArray.js
+++ tasks/rama/3countArray.js
@@ -1,6 +1,12 @@
 function countArray(array) {
   // fungsi perkalian tiap element dalam array
   let hasil = 1; // inisialisasi variabel hasil dari 1
+
+  //Perkondisian untuk cek input harus berupa array
+  //Handling error tampilkan pesan "Input harus berupa array"
+  if (!Array.isArray(array)) {
+    throw new Error("Input harus berupa Array");
+  }
   for (let i = 0; i < array.length; i++) {
     //lakukan perulangan dari index 0 - index terakhir array
     hasil = hasil * array[i]; // lakukan perkalian dari tiap element dalam array
